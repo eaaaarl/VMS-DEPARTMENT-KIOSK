cc19f95b970577d8eff9aee35816d3e12c6cefb7
eaaaarl <dev.earl00@gmail.com>
2025-07-16 11:26:33 +0800

    📦 chore(deps): install expo-orientation for screen orientation control

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,22 @@
 import "@/global.css";
 import { Stack } from "expo-router";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    async function setOrientation() {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.ALL
+      );
+    }
+    setOrientation();
+
+    return () => {
+      ScreenOrientation.unlockAsync();
+    };
+  }, []);
+  
   return (
     <Stack
       screenOptions={{
